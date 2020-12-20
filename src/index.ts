4c1f8ad403d7153b39717f6f12796d9a81a791c2
bjemo1 <brianemo37@yahoo.com>
2020-12-20 09:39:22 -0600

    adding showTrans option

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import bodyParser from 'body-parser';
-import express, { Application } from 'express';
-import transactionCollection from './dbPool';
-import { getTransactions, setupClient } from './plaidService';
-import { getIncomeByQuarter, getIncomeByYear } from './selfEmpService';
+import bodyParser from "body-parser";
+import express, { Application } from "express";
+import transactionCollection from "./dbPool";
+import { getTransactions, setupClient } from "./plaidService";
+import { getIncomeByQuarter, getIncomeByYear } from "./selfEmpService";
 
 setupClient();
 
@@ -11,58 +11,59 @@ const app: Application = express();
 app.use(bodyParser.json());
 
 app.post("*", async (req, res) => {
-    console.log(req.url, req.body);
-    try {
-        if (req.body.webhook_type === 'TRANSACTIONS') {
-            console.log("is transactions")
-            if (req.body.webhook_code === 'DEFAULT_UPDATE') {
-                console.log('is default update')
-                const transactions = await getTransactions();
+  console.log(req.url, req.body);
+  try {
+    if (req.body.webhook_type === "TRANSACTIONS") {
+      console.log("is transactions");
+      if (req.body.webhook_code === "DEFAULT_UPDATE") {
+        console.log("is default update");
+        const transactions = await getTransactions();
 
-                const collection = await transactionCollection();
-                //TODO: Q4 Stuff is duplicating
+        const collection = await transactionCollection();
+        //TODO: Q4 Stuff is duplicating
 
-                const existing = await collection.find({ transaction_id: { $in: transactions.map((t) => t.transaction_id) } }).toArray();
-                const existingIds = existing.map((e) => e.transaction_id);
+        const existing = await collection.find({ transaction_id: { $in: transactions.map((t) => t.transaction_id) } }).toArray();
+        const existingIds = existing.map((e) => e.transaction_id);
 
-                const newOnes = transactions.filter((t) => !existingIds.includes(t.transaction_id));
+        const newOnes = transactions.filter((t) => !existingIds.includes(t.transaction_id));
 
-                if (newOnes.length > 0) {
-                    const insertResult = await collection.insertMany(newOnes);
-                    console.log(insertResult.result);
-                } else {
-                    console.log('nothing new to insert');
-                }
-
-            } else if (req.body.webhook_code === 'TRANSACTIONS_REMOVED') {
-                console.log('is transactions removed')
-                const { removed_transactions } = req.body;
-                console.log(removed_transactions);
-
-                const collection = await transactionCollection();
-
-                await collection.deleteMany({ transaction_id: { $in: removed_transactions } });
-            }
+        if (newOnes.length > 0) {
+          const insertResult = await collection.insertMany(newOnes);
+          console.log(insertResult.result);
+        } else {
+          console.log("nothing new to insert");
         }
+      } else if (req.body.webhook_code === "TRANSACTIONS_REMOVED") {
+        console.log("is transactions removed");
+        const { removed_transactions } = req.body;
+        console.log(removed_transactions);
 
-        res.sendStatus(200);
-    } catch (error) {
-        res.status(500).json(error);
+        const collection = await transactionCollection();
+
+        await collection.deleteMany({ transaction_id: { $in: removed_transactions } });
+      }
     }
+
+    res.sendStatus(200);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-app.get('/incomeByYear/:year', async (req, res, _next) => {
-    const year = parseInt(req.params.year as string);
-    const income = await getIncomeByYear(year);
+app.get("/incomeByYear/:year", async (req, res, _next) => {
+  const year = parseInt(req.params.year as string);
+  const showTrans = !!req.query.showTrans ?? false;
+  const income = await getIncomeByYear(year, showTrans);
 
-    res.json(income);
+  res.json(income);
 });
 
-app.get('/incomeByQuarter/:year', async (req, res, _next) => {
-    const year = parseInt(req.params.year as string);
-    const income = await getIncomeByQuarter(year);
+app.get("/incomeByQuarter/:year", async (req, res, _next) => {
+  const year = parseInt(req.params.year as string);
+  const showTrans = !!req.query.showTrans ?? false;
+  const income = await getIncomeByQuarter(year, showTrans);
 
-    res.json(income);
+  res.json(income);
 });
 
 app.listen(process.env.PORT);
--- src/selfEmpService.ts
+++ src/selfEmpService.ts
@@ -1,149 +1,152 @@
-import transactionCollection from './dbPool';
-import { TransactionPlus } from './plaidService';
-
+import transactionCollection from "./dbPool";
+import { TransactionPlus } from "./plaidService";
 
 enum Month {
-    JAN = 1,
-    FEB,
-    MAR,
-    APR,
-    MAY,
-    JUN,
-    JUL,
-    AUG,
-    SEP,
-    OCT,
-    NOV,
-    DEC
+  JAN = 1,
+  FEB,
+  MAR,
+  APR,
+  MAY,
+  JUN,
+  JUL,
+  AUG,
+  SEP,
+  OCT,
+  NOV,
+  DEC
 }
 
-export async function getIncomeByYear(year: number) {
-    const collection = await transactionCollection();
+export async function getIncomeByYear(year: number, showTrans: boolean) {
+  const collection = await transactionCollection();
 
-    const transactions = (await collection.find<TransactionPlus>()
-        .toArray())
-        .filter((t: TransactionPlus) => {
-            const date = new Date(t.date).getTime();
-            const startOfTaxYear = Date.parse(`1/15/${year}`);
-            const endOfTaxYear = Date.parse(`1/15/${year + 1}`);
+  const transactions = (await collection.find<TransactionPlus>().toArray())
+    .filter((t: TransactionPlus) => {
+      const date = new Date(t.date).getTime();
+      const startOfTaxYear = Date.parse(`1/15/${year}`);
+      const endOfTaxYear = Date.parse(`1/15/${year + 1}`);
 
-            return date >= startOfTaxYear && date < endOfTaxYear;
-        })
-        .sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+      return date >= startOfTaxYear && date < endOfTaxYear;
+    })
+    .sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
 
-    const result: Record<string, { income: number | string, expense: number | string, running: number | string, trans: any[] }> = {};
+  const result: Record<string, { income: number | string; expense: number | string; running: number | string; trans: any[] }> = {};
 
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].forEach((num) => {
-        const yearForKey = num === 13 ? year + 1 : year;
-        const monthForKey = num === 13 ? Month[1] : Month[num];
+  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].forEach((num) => {
+    const yearForKey = num === 13 ? year + 1 : year;
+    const monthForKey = num === 13 ? Month[1] : Month[num];
 
-        const key = `${monthForKey} ${yearForKey}`;
+    const key = `${monthForKey} ${yearForKey}`;
 
-        result[key] = {
-            income: 0,
-            expense: 0,
-            running: 0,
-            trans: []
-        };
-    });
+    result[key] = {
+      income: 0,
+      expense: 0,
+      running: 0,
+      trans: []
+    };
+  });
 
-    transactions.forEach((trans) => {
-        const income = (trans?.amount ?? 0) < 0;
-        const amount: number = Math.abs(trans?.amount ?? 0);
+  transactions.forEach((trans) => {
+    const income = (trans?.amount ?? 0) < 0;
+    const amount: number = Math.abs(trans?.amount ?? 0);
 
-        const key = `${Month[trans.month]} ${trans.fiscal_year}`;
+    const key = `${Month[trans.month]} ${trans.fiscal_year}`;
 
-        result[key].trans.push({
-            amount: (trans.amount! * -1).toFixed(2),
-            date: trans.date
-        });
-
-        if (income === true) {
-            if (key in result) {
-                result[key].income = result[key].income as number + amount;
-            } else {
-                result[key].income = amount;
-            }
-        } else {
-            if (key in result) {
-                result[key].expense = result[key].expense as number + amount;
-            } else {
-                result[key].expense = amount;
-            }
-        }
-    });
-
-    for (const key in result) {
-        result[key].running = (result[key].income as number - (result[key].expense as number)).toFixed(2);
-
-        result[key].income = (result[key].income as number).toFixed(2)
-        result[key].expense = (result[key].expense as number).toFixed(2)
+    if (showTrans) {
+      result[key].trans.push({
+        amount: (trans.amount! * -1).toFixed(2),
+        date: trans.date
+      });
     }
 
-    return result;
+    if (income === true) {
+      if (key in result) {
+        result[key].income = (result[key].income as number) + amount;
+      } else {
+        result[key].income = amount;
+      }
+    } else {
+      if (key in result) {
+        result[key].expense = (result[key].expense as number) + amount;
+      } else {
+        result[key].expense = amount;
+      }
+    }
+  });
+
+  for (const key in result) {
+    result[key].running = ((result[key].income as number) - (result[key].expense as number)).toFixed(2);
+
+    result[key].income = (result[key].income as number).toFixed(2);
+    result[key].expense = (result[key].expense as number).toFixed(2);
+  }
+
+  return result;
 }
 
-export async function getIncomeByQuarter(year: number) {
-    const collection = await transactionCollection();
+export async function getIncomeByQuarter(year: number, showTrans: boolean) {
+  const collection = await transactionCollection();
 
-    const transactions = (await collection.find<TransactionPlus>()
-        .toArray())
-        .filter((t: TransactionPlus) => {
-            if (year !== undefined) {
-                return new Date(t.date).getFullYear() === year;
-            } else {
-                return;
-            }
-        })
-        .sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+  const transactions = (await collection.find<TransactionPlus>().toArray())
+    .filter((t: TransactionPlus) => {
+      if (year !== undefined) {
+        return new Date(t.date).getFullYear() === year;
+      } else {
+        return;
+      }
+    })
+    .sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
 
-    const result: Record<string, { startDate: string, endDate: string, trans: any[], income: number | string, expense: number | string, running: number | string }> = {};
+  const result: Record<
+    string,
+    { startDate: string; endDate: string; trans: any[]; income: number | string; expense: number | string; running: number | string }
+  > = {};
 
-    [1, 2, 3, 4].forEach((num) => {
+  [1, 2, 3, 4].forEach((num) => {
+    const key = `Q${num} ${year}`;
+    result[key] = {
+      startDate: `${1 + (num - 1) * 3}/15/${year}`,
+      endDate: `${(1 + num * 3) % 12}/15/${1 + num * 3 > 12 ? year + 1 : year}`,
+      income: 0,
+      expense: 0,
+      running: 0,
+      trans: []
+    };
+  });
 
-        const key = `Q${num} ${year}`
-        result[key] = {
-            startDate: `${1 + (num - 1) * 3}/15/${year}`,
-            endDate: `${(1 + num * 3) % 12}/15/${(1 + num * 3) > 12 ? year + 1 : year}`,
-            income: 0,
-            expense: 0,
-            running: 0,
-            trans: []
-        };
-    });
+  transactions.forEach((trans) => {
+    const income = (trans?.amount ?? 0) < 0;
+    const amount: number = Math.abs(trans?.amount ?? 0);
 
-    transactions.forEach((trans) => {
-        const income = (trans?.amount ?? 0) < 0;
-        const amount: number = Math.abs(trans?.amount ?? 0);
+    const key = `Q${trans.quarter} ${trans.fiscal_year}`;
 
-        const key = `Q${trans.quarter} ${trans.fiscal_year}`;
-
-        result[key].trans.push({
-            amount: (trans.amount! * -1).toFixed(2),
-            date: trans.date
-        });
-
-        if (income === true) {
-            if (key in result) {
-                result[key].income = result[key].income as number + amount;
-            } else {
-                result[key].income = amount;
-            }
-        } else {
-            if (key in result) {
-                result[key].expense = result[key].expense as number + amount;
-            } else {
-                result[key].expense = amount;
-            }
-        }
-    });
-
-    for (const key in result) {
-        result[key].running = (result[key].income as number - (result[key].expense as number)).toFixed(2);
-
-        result[key].income = (result[key].income as number).toFixed(2)
-        result[key].expense = (result[key].expense as number).toFixed(2)
+    if (showTrans) {
+      result[key].trans.push({
+        amount: (trans.amount! * -1).toFixed(2),
+        date: trans.date
+      });
     }
 
-    return result;
+    if (income === true) {
+      if (key in result) {
+        result[key].income = (result[key].income as number) + amount;
+      } else {
+        result[key].income = amount;
+      }
+    } else {
+      if (key in result) {
+        result[key].expense = (result[key].expense as number) + amount;
+      } else {
+        result[key].expense = amount;
+      }
+    }
+  });
+
+  for (const key in result) {
+    result[key].running = ((result[key].income as number) - (result[key].expense as number)).toFixed(2);
+
+    result[key].income = (result[key].income as number).toFixed(2);
+    result[key].expense = (result[key].expense as number).toFixed(2);
+  }
+
+  return result;
 }
